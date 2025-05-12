d18122d5ed6ab09764d2890ced4a4c9e52ac53e9
Tuomas <32766461+anhava@users.noreply.github.com>
2025-05-12 14:56:07 +0300

    Fix CompanyResearchHome.tsx syntax

--- components/CompanyResearchHome.tsx
+++ components/CompanyResearchHome.tsx
@@ -1,13 +1,18 @@
-// Update the logo references in CompanyResearchHome.tsx
-// Find the img tags with src="/exa_logo.png" and replace them with:
+import React from 'react';
 
-<img 
-  src="/exa-logo-light.svg" 
-  alt="Exa Logo" 
-  className="h-6 sm:h-7 object-contain dark:hidden"
-/>
-<img 
-  src="/exa-logo-dark.svg" 
-  alt="Exa Logo" 
-  className="h-6 sm:h-7 object-contain hidden dark:block"
-/>
+export default function CompanyResearchHome() {
+  return (
+    <div>
+      <img 
+        src="/exa-logo-light.svg" 
+        alt="Exa Logo" 
+        className="h-6 sm:h-7 object-contain dark:hidden"
+      />
+      <img 
+        src="/exa-logo-dark.svg" 
+        alt="Exa Logo" 
+        className="h-6 sm:h-7 object-contain hidden dark:block"
+      />
+    </div>
+  );
+}
